f74e7dcd66f2d469b41dbfeddf0901d495ff20b3
iqbal <iqbalsyamilayas@gmail.com>
2019-08-01 10:02:01 +0000

    update

--- js/main.js
+++ js/main.js
@@ -56,7 +56,7 @@ $(window).load(function(){
 
 
 $('#countdown').countdown({
-	date: "August 1, 2019 06:00:00",
+	date: "August 7, 2019 06:00:00",
 	render: function(data) {
 	  var el = $(this.el);
 	  el.empty()
